bbcd3ccbc2886602de6b62cc4ff264af259aa626
Piyush Jayawant Rewatkar <piyush.jayawant@oneorigin.us>
2025-12-05 15:04:59 -0700

    App name change

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="p-4 border-b bg-card">
       <div className="container mx-auto flex items-center gap-4">
         <h1 className="text-3xl font-bold font-headline text-primary">
-          <Link href="/">DCC Badge Authoring Tool – Development Site</Link>
+          <Link href="/">Credential Co-writer – Development Site</Link>
         </h1>
       </div>
     </header>
